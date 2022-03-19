653612c74381da7686c20e5124caf5e4fb2ac3f3
Daniel Gamboa <danielgrojas94@gmail.com>
2022-03-19 12:38:42 +0100

    Adding dockerfile, docker compose

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+// import express from 'express';
+
+// const app = express();
+// app.listen(4000, () => {
+//   console.log('server running on port 4000');
+// });
+
 import express, { Application } from "express";
 import morgan from "morgan";
 import Router from "./routes";
 
+// const PORT = process.env.PORT || 4000;
 const app: Application = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use(Router);
+
+// app.listen(PORT, () => {
+//   console.log(`Serving on port ${PORT}`);
+// });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./index";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
